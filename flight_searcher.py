9a692df098d18ab93080ca667d97a7f48ad9f841
kevfornal <kevfornal@gmail.com>
2026-07-29 15:56:03 -0400

    Update passenger payload ages in flight searcher

--- flight_searcher.py
+++ flight_searcher.py
@@ -48,8 +48,8 @@ print(f"Generated {len(date_pairs)} date combinations to query.")
 passengers_payload = [
     {"type": "adult"},
     {"type": "adult"},
-    {"type": "child", "age": 8},
-    {"type": "child", "age": 10}
+    {"type": "child", "age": 4},
+    {"type": "child", "age": 6}
 ]
 
 all_offers = []
